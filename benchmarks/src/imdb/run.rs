894a7593b853a0bc3a2e5c6696a9a087b74b82ee
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2024-10-17 22:58:35 -0500

    update

--- benchmarks/src/imdb/run.rs
+++ benchmarks/src/imdb/run.rs
@@ -31,6 +31,7 @@ use datafusion::datasource::listing::{
 };
 use datafusion::datasource::{MemTable, TableProvider};
 use datafusion::error::Result;
+use datafusion::execution::cache::cache_unit::Cache37;
 use datafusion::physical_plan::display::DisplayableExecutionPlan;
 use datafusion::physical_plan::{collect, displayable};
 use datafusion::prelude::*;
@@ -293,7 +294,11 @@ impl RunOpt {
             benchmark_run.start_new_case(&format!("Query {query_id}"));
             let query_run = self.benchmark_query(query_id).await?;
             for iter in query_run {
-                benchmark_run.write_iter(iter.elapsed, iter.row_count);
+                benchmark_run.write_iter(
+                    iter.elapsed,
+                    iter.row_count,
+                    Cache37::consume_bytes_read(),
+                );
             }
         }
         benchmark_run.maybe_write_json(self.output_path.as_ref())?;
